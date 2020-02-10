2303ab4489200e7ec4bd45911dc6c6a0432d79b8
Giuseppe Di Lena <giuseppedilena92@gmail.com>
2020-02-10 12:03:10 +0100

    add object Mapper

--- MaxiNet/Frontend/distriopt.py
+++ MaxiNet/Frontend/distriopt.py
@@ -1,8 +1,23 @@
-class EmbeddedGreedy(object):
+
+
+class Mapper(object):
+    def __init__(self, topo, physical_network_file):
+        self.topo = topo
+        self.physical_network_file = physical_network_file
+
+    @staticmethod
+    def check_valid_path(physical_network_file):
+        pass
+
+
+class EmbeddedGreedy(Mapper):
     pass
 
-class EmbeddedPartitioned(object):
+
+class EmbeddedPartitioned(Mapper):
     pass
 
-class EmbeddedBalanced(object):
+
+class EmbeddedBalanced(Mapper):
     pass
+
